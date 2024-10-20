320ef6be5a5aabe04e380fce35ae6023951c5722
prostarz <tommyspam2000@gmail.com>
2024-10-20 22:28:43 +0100

    feat: complete the rest of creating a plugin + add new routes

--- pages/plugins/_meta.tsx
+++ pages/plugins/_meta.tsx
@@ -1,4 +1,5 @@
 export default {
   "creating-a-plugin": "Creating a plugin",
+  "creating-a-configuration": "Setting up Configuration for your Plugin",
   "installing-a-plugin": "Installing a plugin",
 };
