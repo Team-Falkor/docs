553df82fcd11307abcdf35e916486e3fb1097a67
prostarz <tommyspam2000@gmail.com>
2025-04-04 18:53:34 +0100

    feat(plugin-providers): add guide for creating a plugin with SDK
    Add a new guide for creating a plugin using the Falkor Plugin SDK. This includes a new MDX file with detailed instructions, prerequisites, and API references. Also updated the meta file to include the new guide and bumped dependencies to their latest versions for better compatibility and security.

--- pages/plugin-providers/_meta.tsx
+++ pages/plugin-providers/_meta.tsx
@@ -1,6 +1,8 @@
 export default {
   "creating-a-plugin": "Creating a plugin provider",
+  "creating-a-plugin-with-sdk": "Creating a plugin provider using the sdk",
   "creating-a-configuration":
     "Setting up Configuration for your plugin provider",
   "installing-a-plugin": "Installing a plugin provider",
+  
 };
